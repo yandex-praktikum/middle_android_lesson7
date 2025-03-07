df22b9c4c966fea862a14556777c3e7af69dd867
Дмитрий Гордеев <netimen2@yandex.ru>
2025-03-07 10:29:49 +0300

    audiofocus support

--- app/src/main/java/yandex/practicum/middle/android/ui/MainActivity.kt
+++ app/src/main/java/yandex/practicum/middle/android/ui/MainActivity.kt
@@ -5,27 +5,12 @@ import android.content.ComponentName
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.concurrent.futures.await
 import androidx.lifecycle.lifecycleScope
-import androidx.media3.common.MediaItem
-import androidx.media3.common.Player
 import androidx.media3.session.MediaController
 import androidx.media3.session.SessionToken
-import androidx.media3.ui.PlayerView
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +23,7 @@ class MainActivity : ComponentActivity() {
             MaterialTheme {
                 val player = mediaController.value
                 if (player != null)
-                    Player(player, url)
+                    PlayerScreen(player, url)
             }
         }
     }
@@ -57,36 +42,5 @@ class MainActivity : ComponentActivity() {
 
         mediaController.value?.release()
         mediaController.value = null
-    }
-}
-
-@Composable
-fun Player(player: Player, url: String) {
-    val coroutineScope = rememberCoroutineScope()
-    val mediaItem = remember(url) {
-        MediaItem.Builder().setUri(url).build()
-    }
-
-    LaunchedEffect(mediaItem) {
-        coroutineScope.launch {
-            player.setMediaItem(mediaItem)
-            player.prepare()
-            player.playWhenReady = true
-        }
-    }
-
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Black)
-    ) {
-        AndroidView(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.Center),
-            factory = { ctx ->
-                PlayerView(ctx).apply { this.player = player }
-            }
-        )
     }
 }
